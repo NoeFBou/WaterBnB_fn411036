5e3ec20ab7ece28a930ab2a1c44a02082dd62529
NoeFBou <102726520+NoeFBou@users.noreply.github.com>
2024-12-04 17:38:25 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import json
 import csv
+import os
 
 from flask import request
 from flask import jsonify
@@ -11,9 +12,9 @@ from flask_mqtt import Mqtt
 from flask_pymongo import PyMongo
 from pymongo import MongoClient
 from pymongo.server_api import ServerApi
-
+password = os.getenv("mongoDBpass") 
 ADMIN = False  # Faut être ADMIN/mongo pour écrire dans la base
-uri = "mongodb+srv://noe:kbkqq3942RSWYGX@waterbnb.ti26c.mongodb.net/?retryWrites=true&w=majority&appName=WaterBnB"
+uri = "mongodb+srv://noe:{password}@waterbnb.ti26c.mongodb.net/?retryWrites=true&w=majority&appName=WaterBnB"
 # Create a new client and connect to the  testaled
 client = MongoClient(uri, server_api=ServerApi('1'))
 # -----------------------------------------------------------------------------

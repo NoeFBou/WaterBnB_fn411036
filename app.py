40873008bfcd12272572063e198e529cab4c7c1f
NoeFBou <102726520+NoeFBou@users.noreply.github.com>
2024-12-04 11:31:12 +0100

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ from pymongo import MongoClient
 from pymongo.server_api import ServerApi
 
 ADMIN = True  # Faut être ADMIN/mongo pour écrire dans la base
-uri = "mongodb+srv://noe:<db_password>@waterbnb.ti26c.mongodb.net/?retryWrites=true&w=majority&appName=WaterBnB"
+uri = "mongodb+srv://noe:kbkqq3942RSWYGX@waterbnb.ti26c.mongodb.net/?retryWrites=true&w=majority&appName=WaterBnB"
 # Create a new client and connect to the  test
 client = MongoClient(uri, server_api=ServerApi('1'))
 # -----------------------------------------------------------------------------
